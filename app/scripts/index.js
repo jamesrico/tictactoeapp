0ec9b900016d7b486c886e69cf82c215acca6479
James Rico <jamesrico089@gmail.com>
2015-02-25 13:27:45 -0600

    created game lobby and scss partials

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,7 +3,9 @@
 
 //limit messages to 20
 
-var fb = new Firebase('https://tictactoenssc8.firebaseio.com/');
+var fb = new Firebase('https://tictactoenssc8.firebaseio.com/'),
+    fbusers = new Firebase('https://tictactoenssc8.firebaseio.com/users'),
+    fbgames = new Firebase('https://tictactoenssc8.firebaseio.com/games');
 
 ////////////////////////////////
 // Login/Logout Functionality //
@@ -14,7 +16,8 @@ var fb = new Firebase('https://tictactoenssc8.firebaseio.com/');
     event.preventDefault();
     var $form = $($(this).closest('form')),
         email = $form.find('[type="text"]').val(),
-        pass = $form.find('[type="password"]').val();
+        pass = $form.find('[type="password"]').val(),
+        user = {'user': email};
     fb.createUser({
         email: email+'@tictactoe.com',
         password: pass
@@ -26,6 +29,7 @@ var fb = new Firebase('https://tictactoenssc8.firebaseio.com/');
                 password: pass
               },
                 function(err, auth){
+                    pushLoginInfo(user);
                     location.reload(true);
               }
             );
@@ -40,7 +44,8 @@ var fb = new Firebase('https://tictactoenssc8.firebaseio.com/');
 
     var $form = $($(this).closest('form')),
         email = $form.find('[type="text"]').val(),
-        pass = $form.find('[type="password"]').val();
+        pass = $form.find('[type="password"]').val(),
+        user = {'user': email};
 
     fb.authWithPassword({
       email    : email+'@tictactoe.com',
@@ -49,6 +54,7 @@ var fb = new Firebase('https://tictactoenssc8.firebaseio.com/');
       if (error) {
         console.log("Login Failed!", error);
       } else {
+        pushLoginInfo(user);
         location.reload(true);
         console.log("Authenticated successfully with payload:", authData);
       }
@@ -62,15 +68,52 @@ var fb = new Firebase('https://tictactoenssc8.firebaseio.com/');
 
   //if authenticated, go to app page
 
-  fb.child('users').once('value', function(snap){
-    if (!fb.getAuth()) {
-      $('.login').toggleClass('hidden');
-      $('.app').toggleClass('hidden');
-    } else {}
-  });
+  //fb.child('users').once('value', function(snap){
+    //if (fb.getAuth()) {
+      //$('.login').toggleClass('hidden');
+      //$('.gameSelect').toggleClass('hidden');
+    //} else {}
+  //});
 
+  function pushLoginInfo(user){
+    fbusers.push(user);
+  }
+  ////////creat room functionality
+  //
+  /////push game data to fb
+  function createGame(user){
+    var newGameData = {'user1': user, 'user2': '', 'gameboard': [[0,0,0],[0,0,0],[0,0,0]], 'gameover': false};
+    fbgames.push(newGameData);
+    return newGameData;
+  }
+  ///////append data to list
+  //
+  //grab the entire list of games
+  function getExistingGames(){
+    fb.on('child_added', function (snap) {
+      var game = snap.val(),
+          gameuser = game.user1;
+    });
+  }
+
+  getExistingGames();
+
+  function addGameToList(game){
+    var $li = $('<li class="'+game.user1+'game">'+game.user1+'<p class="'+game.user1+'numberofplayers">1/2</li>');
+    $('.gameListContainer').append($li);
+
+  }
+ $('.addgamebutton').on('click', function(event){
+    event.preventDefault();
+    var userRaw = fb.getAuth().password.email,
+        userIndex = userRaw.indexOf('@'),
+        userShort = userRaw.substr(0,userIndex);
+
+    createGame(userShort);
+    var gameData = createGame(userShort);
+    addGameToList(gameData);
+ })
 ////////////////////////////////////////////////
 //////////// App Functions ////////////////////
 ////////////////////////////////////////////////
-
 
